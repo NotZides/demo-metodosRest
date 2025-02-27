762703b21d82c5d6517f8b4432615b56b81e0426
NotZides <kalesito.u.asesino2003@gmail.com>
2025-02-27 08:52:35 -0500

    initial commit

---
+++ src/test/java/pe/edu/idat/demo_metodosRest/DemoMetodosRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_metodosRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMetodosRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
